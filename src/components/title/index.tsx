9d5bbe242d8ca5f05148c3d85d000e5091fb9d2d
Avarka <vadavar7@gmail.com>
2021-01-14 21:32:41 +0100

    feat(header): logo link to the to feat(header): login button hidden on top feat(smooth-scroll): optional title added feat(title): small redesign feat(title): new style added, responsivity needed! feat: scrollbar hidden

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,22 +1,29 @@
-import { FunctionalComponent, h } from "preact";
+import {FunctionalComponent, h} from "preact";
 import SmoothScroll from "../smooth-scroll";
 import * as style from "./style.css";
 
 const Title: FunctionalComponent = () => {
     return (
-        <div class={style.title}>
-            <a id="Title" />
-            <h1>ParkMyst</h1>
-            <p>
-                Bring people <br /> together with the power <br /> of <span>entertainment</span>
-            </p>
-            <div id="buttons">
-                <SmoothScroll scrollToId="Discover">
-                    <span class={style.roundButton}>Discover</span>
-                </SmoothScroll>
-                <SmoothScroll scrollToId="ContactUs">
-                    <span>Contact us</span>
-                </SmoothScroll>
+        <div className={style.wrapper}>
+            <div className={style.title}>
+                <a id="Title" className={style.anchor}/>
+                <h1>ParkMyst</h1>
+                <p>
+                    Bring people <br/> together with the power <br/> of <span>entertainment</span>
+                </p>
+                <div id="buttons">
+                    <SmoothScroll scrollToId="Discover">
+                        <span className={style.roundButton}>Discover</span>
+                    </SmoothScroll>
+                    <SmoothScroll scrollToId="ContactUs">
+                        <span>Contact us</span>
+                    </SmoothScroll>
+                </div>
+            </div>
+            <div className={style.playButton}>
+                <div>
+                    <a href="https://play.parkmyst.hu">Let's play!</a>
+                </div>
             </div>
         </div>
     );
